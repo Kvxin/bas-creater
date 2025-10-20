9e6aeb06719eee7e30ca9bf2a91d8282643eec55
kvxin <3253261317@qq.com>
2025-10-20 17:21:01 +0800

    feat: 删除ant组件

--- src/composables/useAntdApp.ts
+++
@@ -1,9 +0,0 @@
-import { inject } from 'vue'
-import { AntdAppKey } from '@/plugins/antd-app'
-
-export function useAntdApp() {
-  const api = inject<any>(AntdAppKey)
-  if (!api) throw new Error('useAntdApp() must be used under <AntApp> and <AntdAppProvider>')
-  return api
-}
-
--- src/plugins/antd-app.ts
+++
@@ -1,2 +0,0 @@
-export const AntdAppKey: unique symbol = Symbol('antd-app') as unknown as unique symbol
-
